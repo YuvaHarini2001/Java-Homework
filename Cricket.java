5d843fc4a924878d199ccd8ab77ef6714e0cea54
YuvaHarini2001 <ammuharinibpy2001@gmail.com>
2025-12-27 21:20:56 +0530

    About Java Day 3

---
+++ Cricket.java
@@ -0,0 +1,18 @@
+class Cricket
+{
+	public static void main(String[] args)
+	{
+			String Cricket = "Dhoni finishes off in style";
+			String coffeeshop = "One strong filter coffee";
+			String bankatm = "Balance:$10";
+			String onlineclass = "Mic off, video off, sleeping ON!";
+			String iplfinals = "ee sala cup namdhe!";
+			
+			System.out.println(Cricket);
+			System.out.println(coffeeshop);
+			System.out.println(bankatm);
+			System.out.println(onlineclass);
+			System.out.println(iplfinals);
+		
+	}
+}
---
+++ Java.java
@@ -0,0 +1,18 @@
+class Java
+{
+	public static void main(String[] args)
+	{
+			String food = "i love briyani";
+			String dream = "java sundaresan";
+			String music = "life is better with music!";
+			String canteen = "samosa and chai ready!";
+			String examhall = "no talking, only writing!";
+			
+			System.out.println(food);
+			System.out.println(dream);
+			System.out.println(music);
+			System.out.println(canteen);
+			System.out.println(examhall);
+		
+	}
+}
