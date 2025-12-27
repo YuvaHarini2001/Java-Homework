20101883eb50d37b6c9c4ede1eb138406c2b531f
YuvaHarini2001 <ammuharinibpy2001@gmail.com>
2025-12-27 21:23:23 +0530

    About Java Day 4

---
+++ Amar.java
@@ -0,0 +1,18 @@
+class Amar
+{
+	public static void main(String[] args)
+	{
+			String Amar = "Rules and Regulation kidaiyathu...Irunthal meerappadum...";
+			String Valimai = "No Guts! No Glory!";
+			String Vjs = "Kumutha happy annachi";
+			String Study = "Manama padichen..ana fail aayitten";
+			String Gym = "Naalaikku day 1...Scratch irunthu start pandren";
+			
+			System.out.println(Amar);
+			System.out.println(Valimai);
+			System.out.println(Vjs);
+			System.out.println(Study);
+			System.out.println(Gym);
+		
+	}
+}
---
+++ Rolex.java
@@ -0,0 +1,18 @@
+class Rolex
+{
+	public static void main(String[] args)
+	{
+			String Rolex = "Rolex...Avan peru Dilli...";
+			String Vikram = "Once upon a time...there lived a ghost";
+			String Leo = "Bloody Sweet!";
+			String Jailer = "Hukum...Tiger ka Hukum";
+			String Chitti= "Hi, I am Chitti! Speed 1 terehertz, memory1 zettabyte";
+			
+			System.out.println(Rolex);
+			System.out.println(Vikram);
+			System.out.println(Leo);
+			System.out.println(Jailer);
+			System.out.println(Chitti);
+		
+	}
+}
