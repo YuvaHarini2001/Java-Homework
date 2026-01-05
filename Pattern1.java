dd9fa396724d8158b63c13c73a6a8241d7d9007d
YuvaHarini2001 <ammuharinibpy2001@gmail.com>
2026-01-05 19:22:16 +0530

    Pattern

--- Pattern1.java
+++ Pattern1.java
@@ -4,28 +4,9 @@ class Pattern1
 	{
 		for(int i=1;i<=5;i++)
 		{
-			for(int j=1;j<=i;j++)
+			for(int j =1;j<=i;j++)
 			{
-				if(j<=1)
-				{
-					System.out.print("1");
-				}
-				else if(j<=2)
-				{
-					System.out.print("2");
-				}
-				else if(j<=3)
-				{
-					System.out.print("3");
-				}
-				else if(j<=4)
-				{
-					System.out.print("4");
-				}
-				else if(j<=5)
-				{
-					System.out.print("5");
-				}
+				System.out.print(j);
 			}
 			System.out.println(" ");
 		}
--- Pattern2.java
+++ Pattern2.java
@@ -2,30 +2,11 @@ class Pattern2
 {
 	public static void main(String[] args)
 	{
-		for(int i=1;i<=5;i++)
+		for(int i=5;i>=1;i--)
 		{
-			for(int j=5;j>=i;j--)
+			for(int j=1;j<=i;j++)
 			{
-				if(j>=5)
-				{
-					System.out.print("1");
-				}
-				else if(j>=4)
-				{
-					System.out.print("2");
-				}
-				else if(j>=3)
-				{
-					System.out.print("3");
-				}
-				else if(j>=2)
-				{
-					System.out.print("4");
-				}
-				else if(j>=1)
-				{
-					System.out.print("5");
-				}
+				System.out.print(j);
 			}
 			System.out.println(" ");
 		}
