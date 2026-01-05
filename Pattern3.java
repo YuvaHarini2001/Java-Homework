07123545d75e473a7d7af4c031c27b69b7f9ea9d
YuvaHarini2001 <ammuharinibpy2001@gmail.com>
2026-01-05 17:10:46 +0530

    Pattern

--- Pattern3.java
+++ Pattern3.java
@@ -1,32 +1,39 @@
+import java.util.Scanner;
 class Pattern3
 {
 	public static void main(String[] args)
 	{
+		Scanner sc = new Scanner(System.in);
 		
-		for (int i = 1 ; i<=4; i++ ) 
+		int i, j;
+		
+		System.out.print("Enter the number of Rows :");
+		int rows = sc.nextInt();
+		
+		for (i = 1 ; i <= rows; i++ ) 
 		{
-			for(int s=3;s>=i;s--)
+			for (j = 1 ; j <= rows - i; j++ ) 
 			{
-				System.out.print(" ");
+				System.out.print(" ");	
 			}
-				for(int j=1;j<=(2*i-1);j++)
-				{
-					System.out.print("*");
-				}
-				System.out.println("");
-		}
-		for(int i=5;i<=7;i++)
-		{
-			for(int s=1;s<=(i-4);s++)
-			{
-				System.out.print(" ");
-				
-			}
-			for(int j=15-(2*i);j>=1;j--)
+			for (j = 1 ; j <= i * 2 - 1; j++ ) 
 			{
 				System.out.print("*");
 			}
-			System.out.println("");
+			System.out.println();
+		}
+		
+		for (i = rows - 1 ; i > 0; i-- ) 
+		{
+			for (j = 1 ; j <= rows - i; j++ ) 
+			{
+				System.out.print(" ");
+			}
+			for (j = 1 ; j <= i * 2 - 1; j++ ) 
+			{
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 	}
 }
